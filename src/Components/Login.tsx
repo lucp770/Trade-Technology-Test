9ebc5e7a13d4e588ce6ebd2f4e83f3f83201575f
Lucas P.F <lucp770@gmail.com>
2023-05-20 14:27:14 -0300

    adicionada tela de carregamento com MUI

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,7 +1,10 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
+import { Backdrop } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 
+async function APIKeyValid(apikey: string, userVerification:any, apiKeyDefinition:any, setLoading:any){
 
-async function APIKeyValid(apikey: string, userVerification:any, apiKeyDefinition:any){
+    setLoading(true);
 
     let myHeaders = new Headers();
     myHeaders.append("x-rapidapi-key", apikey);
@@ -21,9 +24,11 @@ async function APIKeyValid(apikey: string, userVerification:any, apiKeyDefinitio
     console.log({resultado});
 
     if(resultado.results ==1){
+        setLoading(false);
         apiKeyDefinition(apikey);
         userVerification(true);
     }else{
+        setLoading(false);
         alert('chave API invalida');
         userVerification(false);
     }
@@ -32,27 +37,35 @@ async function APIKeyValid(apikey: string, userVerification:any, apiKeyDefinitio
 function Login(props:any){
     //cria uma referencia para o input usado para digitar a chave API.
     const {userVerification, apiKeyDefinition} = props;
+    const [Loading, setLoading] = useState(false);
 
     const apiKeyInput:any = useRef();
 
     return(
-        <div className="login-container">
+        <>
+        <div className="main-container">
 
-            <h2> Bem Vindo ao Meu Time</h2>
-
+            <h2> Bem Vindo ao <span className='app-title'>Meu Time</span></h2>
             <h3>O aplicativo favorito dos torcedores fieis</h3>
-            <p>
-                Insira abaixo a chave API obtida da <a href='https://www.api-football.com/documentation-v3'>API-footbal</a>
+            
+            <p>Insira abaixo a chave API obtida da <a href='https://www.api-football.com/documentation-v3'>API-footbal</a>
             </p>
 
             <div className='login-container'>
 
-                <label htmlFor="api-key-input">Chave API:</label>
-                <input type='text' ref={apiKeyInput} />
-                <button className='login-btn' onClick={()=>{APIKeyValid(apiKeyInput.current.value, userVerification, apiKeyDefinition)}}>Login</button>
+                <div className="input-container">
+                    <label htmlFor="api-key-input">Chave API:</label>
+                    <input type='text' ref={apiKeyInput} />
+                </div>
 
+                <button className='login-btn' onClick={()=>{APIKeyValid(apiKeyInput.current.value, userVerification, apiKeyDefinition, setLoading)}}>Entrar</button>
             </div>
         </div>
+        {Loading?
+        <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open >
+            <CircularProgress color="inherit" />
+        </Backdrop> :null  }
+        </>
     )
 }
 
